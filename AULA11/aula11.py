66b75e687d7105debec66a5eda8948d786e6e6e9
laisa4 <laisa.aisha@gmail.com>
2019-11-11 21:59:14 -0300

    aula python

---
+++ AULA11/aula11.py
@@ -0,0 +1,14 @@
+nome = input("Digite seu nome")
+idade = input("Digite a sua idade")
+if nome == 'Laisa' and idade == 26
+    print("Olá, Laisa")
+    elif nome == 'Carolina':
+        print
+else:
+    print("vocÊ não é a Laísa")
+    lista = ['uva', 'banana', 'laranja', 'pessego']
+    #for item in lista:
+
+    #for i in range(0, len(lista)):
+    #   print (lista[i]):
+    

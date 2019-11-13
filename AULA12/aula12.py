3c76a61bb6b115bc04edc6093743a688fca9961e
laisa4 <laisa.aish@gmail.com>
2019-11-12 22:04:04 -0300

    aula finalizada

---
+++ AULA12/aula12.py
@@ -0,0 +1,12 @@
+def somar():
+    num1 = float(input("Digite numero 1"))
+    num2 = float(input("Digite numero 2"))
+    return num1 + num2
+
+
+def subtrair(num1, num2):
+    return num1 - num2
+
+
+print(somar())
+print(subtrair(10.5,5)) 
---
+++ AULA12/oo.py
@@ -0,0 +1,18 @@
+class Mamiferos():
+    olhos = 2
+    patas = 4
+
+    def _init_(self,pelo,especie,rabo,cor):
+    self.pelo = pelo 
+    self.especie = especie
+    self.pelos = pelos
+    self.rabo = rabo
+    self.cor = cor
+
+    def comer ():
+        print("Come")
+    def som ():
+        print("SOM")
+
+mamifero = Mamiferos("curto", "caninos",True, "caramelo")
+manifero2git
